c5941ca212ee739fdf4375e10bd1590d307114c5
alexander keckley <61635682+Drkeck@users.noreply.github.com>
2020-03-31 23:34:04 -0700

    added function tied to button click that adds, fills, and styles new element before adding to list

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,5 +1,12 @@
 console.dir(window.document);
 var buttonE1 = document.querySelector("#save-task");
-buttonE1.addEventListener("click", function() {
-    alert("button clicked");
-});
+var tasksToDoE1 = document.querySelector("#task-to-do");
+
+var createTaskHandler = function() {
+    var listItemE1 = document.createElement("li");
+    listItemE1.className = "task-item";
+    listItemE1.textContent = "This is a new task.";
+    tasksToDoE1.appendChild(listItemE1);
+};
+
+buttonE1.addEventListener("click", createTaskHandler)
